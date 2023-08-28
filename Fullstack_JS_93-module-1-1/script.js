dfc82086a95f8ae1bf5999de120f489ede626304
Kateryna4213 <katyaa6662716@gmail.com>
2023-08-28 19:00:03 +0200

    my addings to js code

--- Fullstack_JS_93-module-1-1/script.js
+++ Fullstack_JS_93-module-1-1/script.js
@@ -1,3 +1,19 @@
 // const a = "new variable"; // ініціалізуємо завжди зі значенням
 
 // console.log(a)
+// let count = 0;
+// while (count < 5) {
+//   console.log(count);
+//   count++;
+// }
+let count = 0;
+
+function delayedWhileLoop() {
+  if (count <= 15) {
+    console.log(count);
+    count++;
+    setTimeout(delayedWhileLoop, 500); // Задержка в 1000 миллисекунд (1 секунда)
+  }
+}
+
+delayedWhileLoop();
